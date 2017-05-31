180ccc88384501ca29e569a4d5fd61453c2bc064
Devin McCormack <tonglab@Euclids.local>
2017-05-31 13:32:34 -0500

    modify to define function
    to match how the game wants you to input the function

--- Level_1/largestsquares.py
+++ Level_1/largestsquares.py
@@ -1,12 +1,17 @@
 area=15342
-areas=[]
-areahold=area
-remain=1
-while remain>0:
-    yards=areahold**.5
-    biglength, remain=divmod(yards,1)
-    bigarea=biglength**2
-    areas.append(bigarea)
-    areahold=areahold-bigarea
-    print (remain)
-print(areas)
+
+def answer(s):
+    areas=[]
+    areahold=s
+    remain=1
+    while remain>0:
+        yards=areahold**.5
+        biglength, remain=divmod(yards,1)
+        bigarea=biglength**2
+        areas.append(bigarea)
+        areahold=areahold-bigarea
+        #print (remain)
+    return areas
+    
+
+print(answer(area))
--- Level_2/binary_tree_indexing.py
+++ Level_2/binary_tree_indexing.py
@@ -1,26 +1,28 @@
 h=5
 q=[19,14,28,31]
 
+def answer(h,q):
+    p=[]
+    for j in q:
+        top=2**h-1
+        if j == top:
+            p.append(-1)
+        else:
+            for i in range(1,h):
+                power=h-i
+                tophalf=top-1
+                bottomhalf=tophalf-(-1+2**power)        
+                if bottomhalf < j < tophalf:
+                    top=tophalf
+                elif j < bottomhalf:
+                    top=bottomhalf
+                elif j in [tophalf, bottomhalf]:
+                    p.append(top)
+                    break
 
-p=[]
-for j in q:
-    top=2**h-1
-    if j == top:
-        p.append(-1)
-    else:
-        for i in range(1,h):
-            power=h-i
-            tophalf=top-1
-            bottomhalf=tophalf-(-1+2**power)        
-            if bottomhalf < j < tophalf:
-                top=tophalf
-            elif j < bottomhalf:
-                top=bottomhalf
-            elif j in [tophalf, bottomhalf]:
-                p.append(top)
-                break
 
+    return p
 
-print(p)
+print(answer(h,q))
 
     
